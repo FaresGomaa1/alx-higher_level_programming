ff97038892ca195a7b91de36006f16ef8377bc2f
FaresGomma1 <faresgomaa336@gmail.com>
2024-07-30 10:32:14 -0700

    Add script to print the title of a Star Wars movie by episode number

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require("request");
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error("Error:", error);
+  } else {
+    try {
+      const data = JSON.parse(body);
+      console.log(data.title);
+    } catch (err) {
+      console.error("Error parsing JSON:", err);
+    }
+  }
+});
